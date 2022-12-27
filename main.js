0262f6fde1e980f11eefc86fb6d3d7d70d26856b
Carlos Alberto <carlosalberto.frontend@gmail.com>
2022-12-27 16:29:55 -0300

    Adição de novos arquivos e atualização home page desktop

--- main.js
+++ main.js
@@ -4,3 +4,23 @@ const menu = document.querySelector('.menu-lateral');
 botaoMenu.addEventListener ('click', () => {
    menu.classList.toggle('menu-lateral--ativo');
 })
+
+let contagem = 1;
+document.getElementById('radio1').checked = true;
+
+setInterval( function () {
+   proximaimagem();
+}, 5000)
+
+function proximaimagem () {
+   contagem++;   
+   if (contagem>4) {
+      contagem = 1;
+   }
+
+   document.getElementById('radio'+ contagem).checked = true;
+   
+}
+
+
+
